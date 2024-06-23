b67a7fa62f0a02fa4640237e3a00c663e7d5cc74
Tejdeep Nookala <iamtejdeepn@gmail.com>
2024-06-24 00:58:38 +0530

    All done, made it into a chrome extension, GGs

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
-let value = document.getElementById("inputBox").value
 let convert = document.getElementById("convert")
 let length = document.getElementById("length")
 let volume = document.getElementById("volume")
 let mass = document.getElementById("mass")
 
+function roundOff(number){
+    return Math.round(number*1000)/1000
+}
+
 function convertLength1(length){
-    return length*3.2808
+    return roundOff(length*3.2808)
 }
 
 function convertVolume1(volume){
-    return volume*0.2641
+    return roundOff(volume*0.2641)
 }
 
 function convertMass1(mass){
-    return mass*2.2046
+    return roundOff(mass*2.2046)
 }
 
 function convertLength2(length){
-    return length/3.2808
+    return roundOff(length/3.2808)
 }
 
 function convertVolume2(volume){
-    return volume/0.2641
+    return roundOff(volume/0.2641)
 }
 
 function convertMass2(mass){
-    return mass/2.2046
+    return roundOff(mass/2.2046)
 }
 
 
+
 convert.addEventListener("click",function(){
-    let value = document.getElementById("inputBox").value
+    console.log("clicked")
+    let value = roundOff(document.getElementById("inputBox").value)
     length.textContent = `${value} meters = ${convertLength1(value)} feet | ${value} feet = ${convertLength2(value)} meters`
+    volume.textContent = `${value} liters = ${convertVolume1(value)} gallons | ${value} gallons = ${convertVolume2(value)} liters`
+    mass.textContent = `${value} kilos = ${convertMass1(value)} pounds | ${value} pounds = ${convertMass2(value)} kilos`
 
 
 })
